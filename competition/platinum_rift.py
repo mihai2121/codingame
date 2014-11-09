24acbc8d256f094dc8447f5d5faf5c7172de7c83
mraxilus <esmith@projectaxil.us>
2014-11-09 01:38:52 +0000

    add initial movement.

--- competition/platinum_rift.py
+++ competition/platinum_rift.py
@@ -1,4 +1,6 @@
+import random as r
 import sys as s
+r.seed()
 
   
 def read_zone(id_, zones):
@@ -11,13 +13,13 @@ def read_zone(id_, zones):
     return match
 
 
-def create_zone(id_, platinum, enemies=0, friends=0, owner=-1, paths=set()):
+def create_zone(id_, platinum, enemies=0, friends=0, owner=-1, paths=None):
     return {
         'enemies': enemies,
         'friends': friends,
         'id': id_,
         'owner': owner,
-        'paths': paths,
+        'paths': paths if paths is not None else set(),
         'platinum': platinum
     }
 
@@ -28,21 +30,58 @@ def write_zone(zone, zones):
     return zones
 
 
+def update_zone(id_, enemies, friends, owner, zones):
+    zone = read_zone(id_, zones)
+    zone['enemies'] = enemies
+    zone['friends'] = friends
+    zone['owner'] = owner
+    return write_zone(zone, zones)
+    
+
+def suggest_move(zone):
+    move, target = '', ''
+    if len(zone['paths']) > 0:
+        target = r.sample(zone['paths'], 1)[0]
+        move = str(zone['friends']) + ' ' + str(zone['id']) + ' ' + str(target)
+    return move
+    
+def is_safe(id_):
+    # ensure enemies are not within one turns' range.
+    pass
+    
+def suggest_spawn(zones):
+    spawn = '1 102'
+    return spawn
+
+
 zones = []
 playerCount, myId, zoneCount, linkCount = [int(i) for i in input().split()]
 for i in range(zoneCount):
     id_, platinum = [int(i) for i in input().split()]
     zone = create_zone(id_, platinum)
-    print(zone, file=s.stderr)
     zones = write_zone(zone, zones)
 for i in range(linkCount):
     source, target = [int(i) for i in input().split()]
     zone = read_zone(source, zones)
     zone['paths'].add(target)
+    print(source, target, zone, file=s.stderr)
     zones = write_zone(zone, zones)
 while 1:
+    moves = []
+    spawns = []
     platinum = int(input())
+    for _ in range(int(platinum / 20)):
+        spawn = suggest_spawn(zones)
+        spawns.append(spawn)
     for i in range(zoneCount):
         zId, ownerId, podsP0, podsP1, podsP2, podsP3 = [int(i) for i in input().split()]
-    print("WAIT")
-    print("1 73")
+        players = [podsP0, podsP1, podsP2, podsP3]
+        enemies = sum(players[:myId] + players[myId + 1:])
+        friends = players[myId]
+        update_zone(zId, enemies, friends, ownerId, zones)
+    for zone in zones:
+        if zone['friends'] > 0:
+            move = suggest_move(zone)
+            moves.append(move)
+    print(' '.join(moves) if len(moves) > 0 else 'WAIT')
+    print(' '.join(spawns) if len(spawns) > 0 else 'WAIT')

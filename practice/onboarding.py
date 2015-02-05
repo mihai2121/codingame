d70134b21b6eaf2b83112deedc8ceda623712c23
mraxilus <esmith@projectaxil.us>
2015-02-05 17:31:08 +0000

    Update documentation for Onboarding

--- practice/onboarding.py
+++ practice/onboarding.py
@@ -1,29 +1,28 @@
-# the goal of this game is to protect a ship from oncoming enemies.
-# each turn a set number of enemies will be within shooting range.
-# each enemy is given a name and a distance from the ship.
-# all of this information is retrieved via stdin in their repective order.
-# to target at shoot an enemy, its name must be printed to stdout.
+# The goal of this game is to protect a ship from oncoming enemies. 
+# Each turn a set number of enemies will be within shooting range. 
+# Each enemy has a name and a distance from the ship. 
+# This information fills the stdin buffer in their respective order. 
+# To target and shoot an enemy, the program must print its name to stdout.
 # 
-# the strategy chosen in this case is to always shoot the enemy that is closest.
-
-# the algorithm for this strategy is contained within a standard game loop.
-# the loop is to run continuall until there are no remaining enemies.
+# The strategy chosen in this case is to always shoot the enemy that is closest.
+# The algorithm for this strategy runs within a standard game loop. 
+# The loop is to run until there are no remaining enemies.
 while True:
-  # each cycle, initially the enemy count is read.
-  # in addition a target object is constructed.
+  # Each cycle, the program reads the initial enemy count. 
+  # It also constructs a target object.
   enemies = int(input())  
   target = {
     'name': '', 
     'distance': -1
   }
   
-  # target an enemy if its distance is smaller than any other enemy.
+  # An enemy is then set as the target object if its distance is smaller than any of the other enemies.
   for enemy in range(enemies):
     name, distance = input().split()
     distance = int(distance)
     if target['distance'] == -1 or distance < target['distance']:
       target['name'], target['distance'] = name, distance
       
-  # by this point the targeted enemy is also the closest to the ship.
-  # shoot the enemy by outputing its name to stdout.
+  # By this point the targeted enemy is also the closest to the ship.
+  # Thus, the program then shoots the enemy by outputting its name to stdout.
   print(target['name'])
